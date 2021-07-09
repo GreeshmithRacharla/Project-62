17a02d2a34a0ed0b7334cf0a6bd2a0523e70c039
GreeshmithRacharla <76734059+GreeshmithRacharla@users.noreply.github.com>
2021-07-09 23:22:04 +0530

    Add files via upload

---
+++ HomeScreen.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+import { createSwitchNavigation , createAppContainer } from 'react-navigation';
+
+export default class HomeScreen {
+  render(){
+    return(
+     <View>
+     
+     </View>
+    )
+  }
+}
+export default HomeScreen;
---
+++ SummaryScreen.js
@@ -0,0 +1,4 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+import { createSwitchNavigation , createAppContainer } from 'react-navigation';
